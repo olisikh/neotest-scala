c08cefd756d6583a4ae3ac3cb92c76df46369f07
Oleksii Lisikh <alisiikh@gmail.com>
2026-02-20 23:35:39 +0100

    rename test files in test-project

--- test-project/zio-test/src/test/scala/com/example/ZioSpec.scala
+++ test-project/zio-test/src/test/scala/com/example/ZioSpec.scala
@@ -4,29 +4,37 @@ import zio.{test => _, _}
 import zio.test._
 import zio.test.Assertion._
 
-object ZioTestSuiteAllSpec extends ZIOSpecDefault {
+object ZioSpec extends ZIOSpecDefault {
 
-  def spec = suiteAll("HelloWorldSuiteAll") {
-    suite("hello suite") {
+  def spec = suite("HelloWorldSpec")(
+    suite("hello - suite") {
       test("hello") {
         for {
           one <- ZIO.succeed(1)
           two <- ZIO.succeed(2)
         } yield assertTrue(one + two == 3)
       }
-    }
-
+    },
     test("failing test") {
       for {
         one <- ZIO.succeed(1)
         two <- ZIO.succeed(2)
       } yield assertTrue(one + two == 2)
-    }
-
+    },
     test("more complex test") {
       for {
         value <- ZIO.none
       } yield assert(value)(isSome(equalTo(3)))
+    },
+    test("crashing test") {
+      for {
+        one <- ZIO.succeed(1)
+        two <- ZIO.succeed(2)
+        _ <- ZIO.fail(new RuntimeException("oh no, boom"))
+      } yield assertTrue(one + two == 3)
+    },
+    test("crashing but no zio") {
+      throw new RuntimeException("babaaah!")
     }
-  }
+  )
 }
