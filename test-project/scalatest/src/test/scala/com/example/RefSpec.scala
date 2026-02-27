30f3c4a7d1c3e5ddd60dedf2712845de2f06baa1
Oleksii Lisikh <alisiikh@gmail.com>
2026-02-27 17:42:35 +0100

    fix(scalatest): make SampleRefSpec discoverable by sbt

--- test-project/scalatest/src/test/scala/com/example/RefSpec.scala
+++ test-project/scalatest/src/test/scala/com/example/RefSpec.scala
@@ -4,18 +4,15 @@ import org.scalatest.refspec.RefSpec
 import org.scalatest.matchers.should.Matchers
 
 class SampleRefSpec extends RefSpec  with Matchers {
+  def `successful example`(): Unit = {
+    10 shouldEqual 10
+  }
 
-  object Testing {
-    def `successful example`(): Unit = {
-      10 shouldEqual 10
-    }
+  def `failing example`(): Unit = {
+    10 shouldEqual 11
+  }
 
-    def `failing example`(): Unit = {
-      10 shouldEqual 11
-    }
-
-    def `crashing example`(): Unit = {
-      throw new RuntimeException("refspec crash")
-    }
+  def `crashing example`(): Unit = {
+    throw new RuntimeException("refspec crash")
   }
 }
